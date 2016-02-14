26366bdce86a5173a30bab34513aca1e3cb21dc1
inversquare <chad.d.lagore@gmail.com>
2016-02-13 16:08:38 -0800

    robot.h new control functions

--- robot.h
+++ robot.h
@@ -35,6 +35,19 @@ namespace Robot {
         float x;
         float y;
     };
+	
+	/*	Strucutre to hold state variables.
+	*	x & y are in centimeters, heading is in radians.
+	*
+	*	This structure should be moved to the header file
+	*	eventually.
+	*/
+	typedef struct State {
+		float x;
+		float y;
+		float heading;	
+	}
+
     
     class AI {
     public:
@@ -122,7 +135,7 @@ namespace Robot {
          *                          If false, the robot will continue in the
          *                          specified direction until instructed otherwise.
          */
-        void go(Vector direction, bool stopAtDestination = false);
+        void go(Vector destination, bool stopAtDestination = false);
         
         /*
          * void stop()
@@ -162,6 +175,59 @@ namespace Robot {
          *                 180
          */
         void orientRangeFinder(int orientation);
+		
+	   /*	void wheelControl(float right_velocity, float left_velocity)
+		*
+		*	Applies PWM signal to wheel motors. Must convert velocities into
+		*   PWM appropriate signal (integer from 0-255)
+		*	
+		*	Parameters:
+		*	float right_velocity: An angular velocity, specified in radians
+		*						  per second, to be applied to the right wheel.
+		*						  This function will assume velocities requested
+		*						  are within the operating range of the motor as 
+		*						  specified in the datasheet. This function must 
+		*						  convert the angular velocity to some integer 
+		*						  between 0 & 255 using a linear model.
+		*
+		*	float left_velocity:  An angular velocity, specified in radians per
+		*						  second, to be applied to the left wheel. Similar
+		*						  conditions to the right_velocity parameter.
+		*/
+		void wheelControl(float right_velocity, float left_velocity);
+
+	   /*	void calculateOdometry(State * state, float left_w, float right_w)
+		*
+		*	Calculates distance travelled in an individual time step and updates the 
+		*	state variables to match. Should be called following each time step so that
+		*	the state variables in the x-y plane and the heading remain up to date.
+		*	
+		*	Parameters: 
+		*	State * state:		A pointer to the state structure.
+		*	float left_w:		(Read "left omega"). The angular velocity of the left 
+		*						during the time step.
+		*	float right_w:		(Read "right omega") The angular velocity of the right
+		*						wheel during the time step.
+		*/
+		void calculateOdometry(State * state, float left_w, float right_w);
+	   
+	   /*	float wheelVelocity(float w, float v, int wheel)	
+		*
+		*	Computes left or right wheel angular velocity given craft desired velocity
+		*  	and angular velocity. Documentation on these formulae is availabile in the
+		*	accompanying report. This abstraction allows us to use a unicycle dynamical
+		*	model instead of a differential drive when making incremental state changes.
+		*	
+		*	Parameters:		
+		*	float w:		("omega") A desired angular velocity of the robot, for one 
+		*					time step.
+		*	float v:		A desired velocity of the robot, for one time step.
+		*		
+		*	Returns:		A wheel velocity in float format for the wheelControl function
+		*					to apply to the wheels, and for the calculateOdometry function
+		*					to use in calculating state changes during a time step.
+		*/
+		float wheelVelocity(float w, float v, int wheel);
     };
     
     /*
